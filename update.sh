a6f2c33e9e28bea73225d124f15cd050120e26fb
Anonymous <user@localhost>
2024-03-01 14:57:13 +0000

    Рефакторинг

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 MY_LOCATION=$(dirname $0)
-OLD_COMMIT=$(cat ${MY_LOCATION}/.git/ORIG_HEAD)
+OLD_COMMIT=$(cat ${MY_LOCATION}/.git/ORIG_HEAD 2>&1)
 # TODO: проверить, может, FETCH_HEAD будет лучше
 git pull -q || exit 1
-NEW_COMMIT=$(cat ${MY_LOCATION}/.git/ORIG_HEAD)
+NEW_COMMIT=$(cat ${MY_LOCATION}/.git/ORIG_HEAD 2>&1)
 
-if ! [[ "${OLD_COMMIT}" == "${NEW_COMMIT}" ]]; then
+if ! test "${OLD_COMMIT}" == "${NEW_COMMIT}"; then
   docker compose up --build --detach --force-recreate
 fi
