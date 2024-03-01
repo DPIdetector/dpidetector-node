f2e3fc40bcd3f9ae35f9c7b4a68969fdcaf03766
Anonymous <user@localhost>
2024-03-01 15:30:49 +0000

    Рефакторинг

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 MY_LOCATION=$(dirname $0)
-OLD_COMMIT=$(cat ${MY_LOCATION}/.git/ORIG_HEAD 2>&1)
+
+OLD_COMMIT=$(test -f ${MY_LOCATION}/.git/ORIG_HEAD && cat ${MY_LOCATION}/.git/ORIG_HEAD)
 # TODO: проверить, может, FETCH_HEAD будет лучше
 git pull -q || exit 1
-NEW_COMMIT=$(cat ${MY_LOCATION}/.git/ORIG_HEAD 2>&1)
+NEW_COMMIT=$(test -f ${MY_LOCATION}/.git/ORIG_HEAD && cat ${MY_LOCATION}/.git/ORIG_HEAD)
 
-if ! test "${OLD_COMMIT}" == "${NEW_COMMIT}"; then
-  docker compose up --build --detach --force-recreate
+if ! [[ "${OLD_COMMIT}" == "${NEW_COMMIT}" ]]; then
+  if [[ -f /usr/libexec/docker/cli-plugins/docker-compose ]]; then
+    docker compose up --build --detach --force-recreate
+  else
+    docker-compose up --build --detach --force-recreate
+  fi
 fi
